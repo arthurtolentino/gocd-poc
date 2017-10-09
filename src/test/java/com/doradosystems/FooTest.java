ec99ab16943a5109bebe2fd0e15c81a20a8ea4d0
arthurtolentino <arthur.tolentino@doradosystems.com>
2017-10-09 10:00:21 -0400

    added unit test

---
+++ src/test/java/com/doradosystems/FooTest.java
@@ -0,0 +1,13 @@
+package com.doradosystems;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class FooTest {
+	
+	@Test
+	public void fooTest(){
+		assertEquals("foo".length(), 3);
+	}
+
+}
