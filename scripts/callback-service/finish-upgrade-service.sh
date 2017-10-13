a70f8c885f45d8f9c554fb82f714207ef428e052
arthurtolentino <arthur.tolentino@doradosystems.com>
2017-10-13 14:02:08 -0400

    parameterize scripts

--- scripts/callback-service/finish-upgrade-service.sh
+++ scripts/callback-service/finish-upgrade-service.sh
@@ -2,12 +2,11 @@
 set -e
 
 retry=0
-max_retry=5
 while true; do
   echo 'waiting for upgrade to complete...'
   response=$(curl \
   -u "$RANCHER_API_USERNAME:$RANCHER_API_PASSWORD" \
-  -X GET 'https://rancher.doradosystems.com:8443/v2-beta/projects/1a11/services/1s424')
+  -X GET "$RANCHER_HOST/v2-beta/projects/1a11/services/$SERVICE_ID")
 
   `# extract and query by state field. rancher api filter does not work at service level`
   serviceState=$(expr "$response" : '.*"state":"\([^"]*\)"')
@@ -16,15 +15,15 @@ while true; do
       echo 'performing finishupgrade...'
       response=$(curl \
       -u "$RANCHER_API_USERNAME:$RANCHER_API_PASSWORD" \
-      -X POST 'https://rancher.doradosystems.com:8443/v2-beta/projects/1a11/services/1s424/?action=finishupgrade')
+      -X POST "$RANCHER_HOST/v2-beta/projects/1a11/services/$SERVICE_ID/?action=finishupgrade")
       break
   fi
 
   retry=$(expr $retry + 1)
-  if [ $retry -eq $max_retry ]; then
+  if [ $retry -eq $POLL_DEPLOY_MAX_RETRY ]; then
     $(rollback)
   else
     echo 'sleeping...'
-    sleep 5
+    sleep $WAIT_INTERVAL_SECONDS
   fi
 done
--- scripts/callback-service/upgrade-service.sh
+++ scripts/callback-service/upgrade-service.sh
@@ -5,12 +5,12 @@ echo 'performing upgrade...'
 
 status=$(curl --write-out %{http_code} --silent --output \
 /dev/null -u "$RANCHER_API_USERNAME:$RANCHER_API_PASSWORD" \
--X POST 'https://rancher.doradosystems.com:8443/v2-beta/projects/1a11/services/1s424/?action=upgrade' \
+-X POST "$RANCHER_HOST/v2-beta/projects/1a11/services/$SERVICE_ID/?action=upgrade" \
 -H 'Accept: application/json' \
 -H 'Content-Type: application/json' \
 --data-binary '@upgrade-callback-service.json')
 
-echo "http status: $status"
+echo "status code: $status"
 
 if [ $status -ne 202 ]; then
 	exit 1
