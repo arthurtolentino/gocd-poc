74cefb607c6d8f0dec769034a86cfcd1ff099f8d
arthurtolentino <arthur.tolentino@doradosystems.com>
2017-10-13 14:26:10 -0400

    verbose logging

--- scripts/callback-service/rollback-service.sh
+++ scripts/callback-service/rollback-service.sh
@@ -6,5 +6,4 @@ echo "service id = $SERVICE_ID"
 echo 'performing rollback...'
 
 (curl -u "$RANCHER_API_USERNAME:$RANCHER_API_PASSWORD" \
--X POST "$RANCHER_HOST/v2-beta/projects/1a11/services/$SERVICE_ID/?action=rollback" \
---write-out %{http_code} --silent --output /dev/null | cat)
+-X POST "$RANCHER_HOST/v2-beta/projects/1a11/services/$SERVICE_ID/?action=rollback" | cat)
