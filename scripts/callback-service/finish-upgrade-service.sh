44e7f57bf8c02a6570c8911f764b466a6dad73d8
arthurtolentino <arthur.tolentino@doradosystems.com>
2017-10-13 13:48:51 -0400

    changed host

--- scripts/callback-service/finish-upgrade-service.sh
+++ scripts/callback-service/finish-upgrade-service.sh
@@ -4,10 +4,10 @@ set -e
 retry=0
 max_retry=5
 while true; do
-  echo 'waiting for upgrade to compelte...'
+  echo 'waiting for upgrade to complete...'
   response=$(curl \
   -u "$RANCHER_API_USERNAME:$RANCHER_API_PASSWORD" \
-  -X GET 'http://rancher.doradosystems.com:8080/v2-beta/projects/1a11/services/1s424')
+  -X GET 'https://rancher.doradosystems.com:8443/v2-beta/projects/1a11/services/1s424')
 
   `# extract and query by state field. rancher api filter does not work at service level`
   serviceState=$(expr "$response" : '.*"state":"\([^"]*\)"')
@@ -16,7 +16,7 @@ while true; do
       echo 'performing finishupgrade...'
       response=$(curl \
       -u "$RANCHER_API_USERNAME:$RANCHER_API_PASSWORD" \
-      -X POST 'http://rancher.doradosystems.com:8080/v2-beta/projects/1a11/services/1s424/?action=finishupgrade')
+      -X POST 'https://rancher.doradosystems.com:8443/v2-beta/projects/1a11/services/1s424/?action=finishupgrade')
       break
   fi
 
